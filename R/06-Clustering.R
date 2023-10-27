9686c95bca50aea32ca3f175e52fc0fba1867045
Melissa Mayen Quiroz <mmayen@lcg.unam.mx>
2023-10-27 03:13:39 -0600

    Asignar las etiquetas de clúster al objeto sce existente

--- R/06-Clustering.R
+++ R/06-Clustering.R
@@ -12,3 +12,6 @@ clusters <- igraph::cluster_louvain(g)
 
 ## Asignación de etiquetas de colores a las células (merged) con su respectivo clúster
 colLabels(merged) <- factor(clusters$membership)
+
+# Asignar las etiquetas de clúster al objeto sce existente
+colData(sce.chimera)$Cluster <- factor(clusters$membership)
