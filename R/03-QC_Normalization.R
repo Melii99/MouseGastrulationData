ae2d8fe8a972c9aba101e1b52ab4d9fce5e1abfd
Melissa Mayen Quiroz <mmayen@lcg.unam.mx>
2023-10-27 02:24:16 -0600

    Quality control and Normalization (logNormCounts)

--- R/03-QC_Normalization.R
+++ R/03-QC_Normalization.R
@@ -3,3 +3,16 @@
 #                 03 - Quality Control and Normalization
 
 ################################################################################
+
+## Filtrar y eliminar las células ya marcadas como "stripped" o "Doublet" ##
+
+## Mapear las células marcadas como "stripped" o "Doublet"
+drop <- sce.chimera$celltype.mapped %in% c("stripped", "Doublet")
+## Filtrar estas células (eliminarlas del objeto sce)
+sce.chimera <- sce.chimera[, !drop]
+
+
+## Normalizacion ##
+
+## Calcular valores de expresión logaritmica normalizados
+sce.chimera <- logNormCounts(sce.chimera)
